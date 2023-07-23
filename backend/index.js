79fcfaa1aeb91aeda367e0325ac641371a3e9e70
Glenn Chiang <glennchiang24@gmail.com>
2023-07-23 14:33:23 +0800

    Updated route handlers for PUT and DELETE requests to integrate with MongoDB Added .rest requests for testing

--- backend/index.js
+++ backend/index.js
@@ -9,18 +9,29 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/posts", (req, res) => {
-  Post.find({}).then((posts) => {
-    res.json(posts);
-  });
+  Post.find({})
+    .then((posts) => {
+      res.json(posts);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).end();
+    });
 });
 
-app.get('/posts/:id', (req, res) => {
-  Post.findById(req.params.id).then(post => {
-    res.json(post)
-  })
-})
+app.get("/posts/:id", (req, res, next) => {
+  Post.findById(req.params.id)
+    .then((post) => {
+      if (post) {
+        res.json(post);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
+});
 
-app.post("/posts", (req, res) => {
+app.post("/posts", (req, res, next) => {
   const body = req.body;
 
   if (!body.title) {
@@ -31,32 +42,54 @@ app.post("/posts", (req, res) => {
     return res.status(400).json({ error: "content missing" });
   }
 
-  const newPost = new Post({ ...body });
+  const newPost = new Post({ ...body, dateAdded: new Date(), lastUpdated: new Date() });
 
   newPost.save().then((savedPost) => {
     res.json(savedPost);
-  });
+  }).catch(error => next(error));
 });
 
-app.put("/posts/:id", (req, res) => {
+app.put("/posts/:id", (req, res, next) => {
   const body = req.body;
-  const id = req.params.id;
 
   if (!body.title) {
     return res.status(400).json({ error: "title missing" });
   }
-
   if (!body.content) {
     return res.status(400).json({ error: "content missing" });
   }
 
-  res.json;
+  const post = {...body, lastUpdated: new Date()}
+
+  Post.findByIdAndUpdate(req.params.id, post, {new: true}).then(
+    updatedPost => res.json(updatedPost)
+  ).catch(error => next(error))
 });
 
-app.delete("/posts/:id", (req, res) => {
-  const id = Number(req.params.id);
-  res.status(204).end(); // Doesn't return data
+app.delete("/posts/:id", (req, res, next) => {
+  Post.findByIdAndRemove(req.params.id).then(
+    result => res.status(204).end()
+  ).catch(error => next(error))
 });
+
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({error: 'unknown endpoint'})
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return res.status(400).send({error: 'malformed id'})
+  }
+
+  next(error) // Pass to next error handler
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
