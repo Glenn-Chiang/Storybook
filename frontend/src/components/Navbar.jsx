b8ceca78ea293b6b2bfd268d93207508d6fda310
Glenn Chiang <glennchiang24@gmail.com>
2023-08-02 22:16:16 +0800

    Created navbar dropdown with links to Posts, Users and Groups pages, which have yet to be created

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,21 +1,25 @@
 /* eslint-disable react/prop-types */
 import {
+  faBookOpen,
   faBookReader,
   faSignIn,
+  faUser,
   faUserCircle,
+  faUserGroup,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import userService from "../services/userService";
 
 export default function Navbar() {
   const currentUser = userService.getCurrentUser();
 
   return (
-    <nav className="text-xl flex justify-between fixed w-full h-10 bg-sky-200 text-sky-500 z-10">
+    <nav className="flex justify-between fixed w-full h-10 bg-sky-200 text-sky-500 z-10">
       <NavItem to={"/posts"}>
         <FontAwesomeIcon icon={faBookReader} />
         StoryBook
+        <NavDropdown />
       </NavItem>
       {currentUser ? (
         <NavItem to={`users/${currentUser.userId}/profile`}>
@@ -36,9 +40,41 @@ function NavItem({ to, children }) {
   return (
     <NavLink
       to={to}
-      className={"p-4 flex gap-2 items-center hover:bg-sky-300 hover:text-sky-600"}
+      className={
+        "p-4 text-xl flex gap-2 items-center hover:bg-sky-300 hover:text-sky-600 group"
+      }
     >
       {children}
     </NavLink>
+  );
+}
+
+function NavDropdown() {
+  return (
+    <nav className="absolute top-10 left-0 flex-col hidden group-hover:flex text-lg bg-sky-200 rounded">
+      <DropdownLink to={"/posts"}>
+        <FontAwesomeIcon icon={faBookOpen} />
+        Posts
+      </DropdownLink>
+      <DropdownLink to={"/users"}>
+        <FontAwesomeIcon icon={faUser} />
+        Users
+      </DropdownLink>
+      <DropdownLink to={"/groups"}>
+        <FontAwesomeIcon icon={faUserGroup} />
+        Groups
+      </DropdownLink>
+    </nav>
+  );
+}
+
+function DropdownLink({ children, to }) {
+  return (
+    <Link
+      className="flex gap-2 items-center justify-center p-2 hover:bg-sky-300 hover:text-sky-600 text-sky-500"
+      to={to}
+    >
+      {children}
+    </Link>
   );
 }
