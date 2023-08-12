dbdc76aeacfe8c9a1bb69f94d6746c0408936443
Glenn Chiang <glennchiang24@gmail.com>
2023-08-12 12:15:19 +0800

    Fixed link button styling

--- frontend/src/components/LinkButton.jsx
+++ frontend/src/components/LinkButton.jsx
@@ -6,10 +6,10 @@ export function LinkButton({ route, children }) {
     <NavLink
       to={route}
       className={({ isActive }) =>
-        `bg-sky-200 text-sky-500 w-28 h-28 text-center flex flex-col justify-center items-center gap-2 p-4 rounded-xl ${
+        `w-28 h-28 text-center flex flex-col justify-center items-center gap-2 p-4 rounded-xl ${
           isActive
             ? "bg-sky-400 text-sky-100"
-            : "hover:bg-sky-300 hover:text-sky-600"
+            : "bg-sky-200 text-sky-500 hover:bg-sky-300 hover:text-sky-600"
         }`
       }
     >
